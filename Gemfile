811be27ba74bfba95611641ce9a57f8f81f4a5d5
Tae Yun <yuntjs@gmail.com>
2016-12-15 21:54:51 -0800

    add simple cov to project with sample test for user model

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'devise'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-group :development, :test do
+group :development do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:teacher) { User.create(dob: "02/02/1999",first_name: "Tae", last_name: "Yun", email: "tae@gmail.com", password: "password", password_confirmation: "password") }
+
+  describe "attributes" do
+    it "has a first_name" do
+      expect(teacher.first_name).to eq "Tae"
+    end
+    it "has a last_name" do
+      expect(teacher.last_name).to eq "Yun"
+    end
+    it "has an email" do
+      expect(teacher.email).to eq "tae@gmail.com"
+    end
+    it "has a date of birth" do
+      expect(teacher.dob).to eq Date.new(1999,2,2)
+    end
+  end
 end
