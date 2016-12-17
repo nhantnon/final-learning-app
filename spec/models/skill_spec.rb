f0f281fbc6a94f79f63a70275bb461b0c77604c4
Cory Mathis & Nhan Nguyen <pair+coryrmathis.nhantnon@devbootcamp.com>
2016-12-16 16:06:46 -0800

    add spec tests and pass

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:skill) { Skill.create(name: "singing") }
+
+  describe "attributes" do
+    it "has a name" do
+      expect(skill.name).to eq "singing"
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:teacher) { User.create(dob: "02/02/1999",first_name: "Tae", last_name: "Yun", email: "tae@gmail.com", password: "password", password_confirmation: "password") }
+  let(:user) { User.create(dob: "02/02/1999",first_name: "Tae", last_name: "Yun", email: "tae@gmail.com", password: "password", password_confirmation: "password", location: "92122") }
+  let!(:skill) { user.skills.create!(name: "singing") }
 
   describe "attributes" do
     it "has a first_name" do
-      expect(teacher.first_name).to eq "Tae"
+      expect(user.first_name).to eq "Tae"
     end
     it "has a last_name" do
-      expect(teacher.last_name).to eq "Yun"
+      expect(user.last_name).to eq "Yun"
     end
     it "has an email" do
-      expect(teacher.email).to eq "tae@gmail.com"
+      expect(user.email).to eq "tae@gmail.com"
     end
     it "has a date of birth" do
-      expect(teacher.dob).to eq Date.new(1999,2,2)
+      expect(user.dob).to eq Date.new(1999,2,2)
+    end
+    it "has a location" do
+      expect(user.location).to eq "92122"
+    end
+  end
+
+  describe "association with skills table" do
+    it "has skills" do
+      expect(user.skills.first.name).to eq "singing"
     end
   end
 end
