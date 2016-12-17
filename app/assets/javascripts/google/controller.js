a798d28c38da74b30bc76c4ca1f5a37b9d7fc9e2
Tae Yun <email": null>
2016-12-16 19:59:27 -0800

    currently trying to fixate map view to current location

--- app/assets/javascripts/google/controller.js
+++ app/assets/javascripts/google/controller.js
@@ -19,12 +19,12 @@ Controller.prototype.handlePins = function(){
     that.currentPins = responses;
     console.log(responses);
     var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    // center: {lat: -34.397, lng: 150.644}
+    zoom: 8
   });
     var geocoder = new google.maps.Geocoder();
     geocodeAddress(geocoder, map, responses);
   })
+
 }
 
 function geocodeAddress(geocoder, resultsMap, responses) {
@@ -33,18 +33,18 @@ function geocodeAddress(geocoder, resultsMap, responses) {
   console.log(responses);
   // var address = currentPins[0].address;
   for(var i = 0; i < currentPins.length; i++) {
-  geocoder.geocode({'address': currentPins[i].location}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+    geocoder.geocode({'address': currentPins[i].location}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
 }
 
 
@@ -55,7 +55,7 @@ function initMap() {
   });
   var geocoder = new google.maps.Geocoder();
   var infoWindow = new google.maps.InfoWindow({map: map});
-
+  console.log(navigator.geolocation);
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
@@ -70,10 +70,6 @@ function initMap() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   }
-
-  // document.getElementById('submit').addEventListener('click', function() {
-  //   geocodeAddress(geocoder, map);
-  // });
 }
 
 
