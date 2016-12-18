882841a2658dce15ec8c0ebfa44148de84fe3f2e
Matthew Dimesky <matthew.dimesky@gmail.com>
2016-12-17 16:56:39 -0800

    squash zip code bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,10 @@ skills.each do |skill|
   Skill.create!(name: skill)
 end
 
-User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: Faker::Address.zip)
-User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: Faker::Address.zip)
-User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: Faker::Address.zip)
+User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: '27 walton drive west hartford ct 06107')
+User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: '6009 clerkenwell ct burke va 22015')
+User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: '949 laws ct valley park MO 63088')
+User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: '29445')
+User.create!(dob: "02/02/02", first_name: Faker::Name.first_name,  last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", location: '29445')
 
 
