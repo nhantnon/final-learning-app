a11c6b054c8137161c03280ebc230a034ae13216
Nhan <nhansphotoblog@gmail.com>
2016-12-19 17:54:06 -0800

    add levels to schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.delete_all
 Skill.delete_all
 
-
+User.create(dob: "01/01/01",first_name: "test", last_name: "test", email: "test@test.com", password: "testing", password_confirmation: "testing", location: "92122")
 tae = User.create(dob: "02/02/02",first_name: "Tae", last_name: "Yun", email: "tae@gmail.com", password: "password", password_confirmation: "password", location: Faker::Address.zip)
 @skills = ["Sports", "Language", "Music", "Philosophy", "Technology", "Math", "Science", "The Arts"]
 @skills.each do |skill|
