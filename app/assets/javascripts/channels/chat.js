e2cd8785998f64f6db89feb69a0d0662b295b1f7
Cory Mathis <coryrmathis@gmail.com>
2016-12-21 10:10:52 -0800

    Simple visual formatting of chat.

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -1,7 +1,7 @@
 App.chat = App.cable.subscriptions.create('ChatChannel', {
   received: function(data) {
     $("#chat-container").animate({ scrollTop: $(document).height() }, "slow");
-    return $("[data-chatroom=" + data.sender.id + "_" + data.reciever.id+"]").append("<li>" + data.username + ": " + data.content + "</li>") &&  $("[data-chatroom=" + data.reciever.id + "_" + data.sender.id+"]").append("<li>" + data.username + ": " + data.content + "</li>")
+    return $("[data-chatroom=" + data.sender.id + "_" + data.reciever.id+"]").append("<li>" + "<b>"+ data.username + "</b>" + ": " + data.content + "</li>") &&  $("[data-chatroom=" + data.reciever.id + "_" + data.sender.id+"]").append("<li>" + data.username + ": " + data.content + "</li>")
 
   }
 
